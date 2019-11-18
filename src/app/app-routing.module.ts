c1ded146b1434fa34b64963833a2e8661038f3e5
sebfoufou <fournis.sebastien@gmail.com>
2019-11-18 11:53:07 +0100

    app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { ModifprofilComponent } from './modifprofil/modifprofil.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 
 const routes: Routes = [
   { path: 'inscription', component: InscriptionComponent },
-  { path: 'modif_profil', component: ModifprofilComponent } 
+  { path: 'modif_profil', component: ModifprofilComponent },
+  { path: 'connexion', component: ConnexionComponent },
 ];
 
 @NgModule({
