26d035230ab98b7c95b3ff63a57b2b6d356631db
AubinM92 <aubin.mottier@gmail.com>
2019-11-18 12:21:01 +0100

    Inscription

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.css']
 })
+
 export class InscriptionComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(
+  ) {  }
 
   ngOnInit() {
   }
