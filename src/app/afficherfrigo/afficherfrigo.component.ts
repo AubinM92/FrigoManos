ca32fe7ca194d9f19eb5f8aff3238d4cc0a975c4
J-Janar <jeyamjanar@outlook.fr>
2019-11-18 18:59:52 +0100

    vald

--- src/app/afficherfrigo/afficherfrigo.component.ts
+++ src/app/afficherfrigo/afficherfrigo.component.ts
@@ -1,17 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
+import { Ingredient} from '../model/Ingredient';
+import { Liste } from '../model/Liste';
 
 @Component({
   selector: 'app-afficherfrigo',
   templateUrl: './afficherfrigo.component.html',
   styleUrls: ['./afficherfrigo.component.css']
 })
+
 export class AfficherfrigoComponent implements OnInit {
+
+  user: User= new User();
+  ingredient: Ingredient= new Ingredient();
+  nom;
+  data;
+  http;
+  retour;
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  
+  afficherFrigo() {
+
+    this.user.id = parseInt(localStorage.getItem("id"));
+
+    this.ingredient.nom = this.nom;
+
+    this.http.get('http://localhost:8087/elemFrigo' + this.ingredient.nom, this.ingredient.nom).subscribe(
+      reponse=> {
+        this.data = reponse;
+        console.log(this.data);}
+    )
+
+  }
 
 }
