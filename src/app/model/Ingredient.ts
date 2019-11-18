719848543bb0ef0ac5938ef72982f0f8d3bad0d4
J-Janar <jeyamjanar@outlook.fr>
2019-11-18 17:25:46 +0100

    addmodel2

---
+++ src/app/model/Ingredient.ts
@@ -0,0 +1,19 @@
+export class Ingredient{
+
+id : number;
+nom : string;
+unite: string;
+saison: string;
+
+    constructor(
+
+    ){}
+
+    public Ingredient(nom, unite, saison){
+        this.nom = nom;
+        this.unite = unite;
+        this.saison= saison;
+    }
+
+
+}
