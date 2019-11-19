ee955f1911677be3b322b598952ed6fdbe8b9bc6
AubinM92 <aubin.mottier@gmail.com>
2019-11-19 18:27:55 +0100

    Ajout element

--- src/app/ajouter-element-liste/ajouter-element-liste.component.ts
+++ src/app/ajouter-element-liste/ajouter-element-liste.component.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 import { FormControl } from '@angular/forms';
 import { Ingredient } from '../model/Ingredient';
+import { logging } from 'protractor';
 
 @Component({
   selector: 'app-ajouter-element-liste',
@@ -19,21 +20,18 @@ export class AjouterElementListeComponent implements OnInit {
   options: string[] = [];
   noms: string[] = [];
   filteredOptions: Observable<string[]>;
-
+  ing;
   response;
   ingredient: Ingredient = new Ingredient();
+  idIngredient: number = null;
+  quantite;
 
-
+  element: ElementListe = new ElementListe();
 
   constructor(public dialogRef: MatDialogRef<AjouterElementListeComponent>, private http: HttpClient) {}
 
   ngOnInit() {
     this.recupIngredients();
-    this.filteredOptions = this.myControl.valueChanges
-      .pipe(
-        startWith(''),
-        map(value => this._filter(value))
-      );
   }
   
 
@@ -45,6 +43,11 @@ export class AjouterElementListeComponent implements OnInit {
   }
 
 recupIngredients(){
+  this.filteredOptions = this.myControl.valueChanges
+  .pipe(
+    startWith(''),
+    map(value => this._filter(value))
+  );
   const del = this.http.get('http://localhost:8087/nom-ingredient').toPromise();
   del.then(
     data => {
@@ -62,7 +65,33 @@ recupIngredients(){
   }
 
   valider() {
- 
+
+    const del = this.http.get('http://localhost:8087/nom-ingredient/'+ this.ing).toPromise();
+  del.then(
+    data => {
+      this.response = data;
+      this.idIngredient = this.response;
+      if(this.idIngredient!=null){
+        this.enregistrer();
+      }
+    }, err => {
+      console.log(err);
+    }
+  );
+
   }
+
+enregistrer(){
+  this.element.quantite = this.quantite;
+  this.element.ingredient.id = this.idIngredient;
+  this.element.liste.id = parseInt(localStorage.getItem("vueListe"));
+
+    this.http.post('http://localhost:8087/elemListe', this.element).subscribe(
+      datas=>{
+        this.dialogRef.close();
+    })
+
+    
+}
 
 }
