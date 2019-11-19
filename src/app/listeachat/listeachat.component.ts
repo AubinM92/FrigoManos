e845042c660902e96c40395ca2fccc36a3ce73a8
tmpdevfr <thibault.mp@sfr.fr>
2019-11-19 12:12:59 +0100

    liste achat 2

--- src/app/listeachat/listeachat.component.ts
+++ src/app/listeachat/listeachat.component.ts
@@ -10,13 +10,17 @@ import { HttpClient } from '@angular/common/http';
 export class ListeachatComponent implements OnInit {
 
   mesListes;
+  liste;
 
   constructor(private http : HttpClient) { }
 
   ngOnInit() {
-    this.http.get('http://localhost:8087/liste-globale/' + "").subscribe(
+    this.http.get('http://localhost:8087/liste-globale/' + "2").subscribe(
       data => {
         this.mesListes = data;
+        this.mesListes.forEach(liste => {
+          console.log(liste);
+        });
         console.log(this.mesListes);
       }
     )
