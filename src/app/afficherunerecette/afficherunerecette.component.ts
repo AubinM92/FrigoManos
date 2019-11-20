aa277c7e7154999596c82f27555783a748c502b0
Ysydra <bruna.seferian@gmail.com>
2019-11-20 12:49:12 +0100

    afficher une recette en cours...

--- src/app/afficherunerecette/afficherunerecette.component.ts
+++ src/app/afficherunerecette/afficherunerecette.component.ts
@@ -9,11 +9,16 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./afficherunerecette.component.css']
 })
 export class AfficherunerecetteComponent implements OnInit {
-laRecette;
-  constructor(private recetteService : UnerecetteService, private http: HttpClient) { }
+  laRecette;
+  constructor(private recetteService: UnerecetteService, private http: HttpClient) { }
 
   ngOnInit() {
-this.laRecette = this.recetteService.recette;
+    this.laRecette = this.recetteService.recette;
+    this.http.get('http://localhost:8087/recette/' + this.laRecette.id).subscribe(
+      data => {
+        this.laRecette = data;
+        console.log(this.laRecette);
+      })
   }
 
 }
