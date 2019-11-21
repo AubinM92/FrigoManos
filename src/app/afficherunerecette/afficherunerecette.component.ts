9fb2a5f1fbf041fe50f2bcdfe476985eabf9526c
Ysydra <bruna.seferian@gmail.com>
2019-11-21 15:38:43 +0100

    afficher une recette boutons...

--- src/app/afficherunerecette/afficherunerecette.component.ts
+++ src/app/afficherunerecette/afficherunerecette.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { UnerecetteService } from '../unerecette.service';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
 import { ElementFinder } from 'protractor';
+import { Envie } from '../model/Envie';
+import { Liste } from '../model/Liste';
+import { ChoixajoutrecettelisteService } from '../choixajoutrecetteliste.service';
+import { ChoixajoutrecettelisteComponent } from '../choixajoutrecetteliste/choixajoutrecetteliste.component';
 
 @Component({
   selector: 'app-afficherunerecette',
@@ -20,7 +24,15 @@ export class AfficherunerecetteComponent implements OnInit {
   pasAssez = 0;
   elemTrouve;
 
-  constructor(private recetteService: UnerecetteService, private http: HttpClient) { }
+  dateAuj;
+  nouvelleEnvie : Envie = new Envie();
+  recetteEnvie;
+  message;
+  VerifAjoutEnvie;
+  listeRecette : Liste = new Liste();
+
+
+  constructor(private recetteService: UnerecetteService, private http: HttpClient, private ajoutService: ChoixajoutrecettelisteService,private dialog2: MatDialog) { }
 
   ngOnInit() {
 
@@ -85,5 +97,33 @@ export class AfficherunerecetteComponent implements OnInit {
     if (this.elemTrouve.quantite < elemRe.quantite) {
       this.pasAssez = 1;
     }
+  }
+
+  ajouterEnvie(re){
+    this.dateAuj = this.maDate();
+    this.nouvelleEnvie.date = this.dateAuj;
+    this.nouvelleEnvie.recette = re;
+    this.nouvelleEnvie.user.id = localStorage.id;
+    const del = this.http.post('http://localhost:8087/envie', this.nouvelleEnvie).toPromise()
+    del.then(data =>{
+    this.VerifAjoutEnvie = data;
+    });
+    if(this.VerifAjoutEnvie !=null){
+      this.message = "Recette ajoutée aux envies"
+    } else {
+      this.message = "Encore une fois ?!"
+    }
+  }
+
+  ajouterRecetteCourse(re){
+    this.listeRecette.titre = re.titre;
+    this.listeRecette.user.id= localStorage.id;
+    this.ajoutService.recette=re;
+    this.ajoutService.liste = this.listeRecette;
+    const mydial2 = this.dialog2.open(ChoixajoutrecettelisteComponent);
+  }
+
+  maDate(){
+    return new Date();
   }
 }
