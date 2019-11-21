8660e519ed406fe8b1b77157c865ed4220201673
sebfoufou <fournis.sebastien@gmail.com>
2019-11-21 16:43:59 +0100

    dsfsd

--- src/app/afficherenvie/afficherenvie.component.ts
+++ src/app/afficherenvie/afficherenvie.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-afficherenvie',
@@ -7,11 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AfficherenvieComponent implements OnInit {
 
+  lesRecettes;
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
 
   ngOnInit() {
-
+    this.http.get('http://localhost:8087/recetteByEnvieByUser/' +localStorage.id).subscribe(
+      data => {
+        this.lesRecettes= data;
+      })
   }
 
 }
